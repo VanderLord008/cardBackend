97cb185a9997f654a1ef2323643edfccb4ba2544
VanderLord008 <90377070+VanderLord008@users.noreply.github.com>
2024-01-03 18:05:39 +0530

    Update socketMain.js

--- socketMain.js
+++ socketMain.js
@@ -5,12 +5,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+  const allowedOrigins = ['http://localhost:5000', 'https://cardbackend.onrender.com','https://tubular-moonbeam-71e070.netlify.app/'];
+      const origin = req.headers.origin;
+      if (allowedOrigins.includes(origin)) {
+           res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+      res.header("Access-Control-Allow-credentials", true);
+      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+      next();
 });
 const PORT = 9000;
 const expressServer = app.listen(PORT);
@@ -18,7 +21,7 @@ const expressServer = app.listen(PORT);
 const socketio = require("socket.io");
 const io = socketio(expressServer, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   },
 });
